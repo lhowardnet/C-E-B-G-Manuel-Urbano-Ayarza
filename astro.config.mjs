8202f2f5e64d5c21aeeefbf99681f1681bdf049c
Luis Howard <profesionalhoward507@outlook.com>
2025-12-17 18:41:04 -0500

    Update site URL and image format in configuration and structured data

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://manuelubanayarza.pa',
+  site: 'https://escuela-manuel-urbano-ayarza.com',
   vite: {
     plugins: [tailwindcss()]
   },
