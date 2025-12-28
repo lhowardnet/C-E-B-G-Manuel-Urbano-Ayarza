ea0baa5ac7c4c13a25cd198ac9831414c1933b08
Luis Howard <profesionalhoward507@outlook.com>
2025-12-28 15:01:10 -0500

    Remove trailing slash from site URL in astro.config and remove unused BoxTitle import in robotics.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.escuela-manuel-urbano-ayarza.com/',
+  site: 'https://www.escuela-manuel-urbano-ayarza.com',
   trailingSlash: 'never',
   vite: {
     plugins: [tailwindcss()]
